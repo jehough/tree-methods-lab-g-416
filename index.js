a2f256b34e3ffd67aadc6cbc65b37b306e56d3c9
Jim Hough <teachlife@outlook.com>
2020-06-02 13:26:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ function inOrder(currentNode){
   if(currentNode.left){
     inOrder(currentNode.left)
   }
+  console.log(currentNode)
+  if(currentNode.right){
+    inOrder(currentNode.left)
+  }
 }
